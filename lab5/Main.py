ea4aab9123e67e0ba4063ad29d852d7ad6b98e24
FullBettary <whitecoon9@gmail.com>
2022-11-26 13:56:09 +0300

    append tabel of coordinate for lab 5

--- lab5/Main.py
+++ lab5/Main.py
@@ -5,8 +5,6 @@ WIDTH, HEIGHT = 910, 650
 SIZE = (WIDTH, HEIGHT)
 LIGHT_GREY = (160, 160, 160)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 250, 0)
 FPS = 60
 pixel_size = 10
 
@@ -26,12 +24,15 @@ def draw_coord_axies():
         text = font.render(f'{i // pixel_size}', True, BLACK)
         sf.blit(text, (10, i))
 
-def draw_line(x1, y1, x2, y2):
+def draw_line(x1, y1, x2, y2, flag):
     x1, y1, x2, y2 = x1 * pixel_size, y1 * pixel_size, x2 * pixel_size, y2 * pixel_size
     x, y = x1, y1
     dx, dy = (x2 - x1)//pixel_size, (y2 - y1)//pixel_size
     e = 2 * dx - dy
+    print("Line coordinate") if not flag else None
     for i in range(dy + 1):
+        if not flag: print(f'({x // pixel_size}, {y // pixel_size})')
+
         pg.draw.rect(sf, BLACK, (x, y, pixel_size, pixel_size))
         while e >= 0:
             x += pixel_size
@@ -39,16 +40,24 @@ def draw_line(x1, y1, x2, y2):
         e += 2 * dx
         y += pixel_size
 
-def draw_circle(x, y, r):
+
+def draw_circle(x, y, r, flag):
     disp_x, disp_y, r = x * pixel_size, y * pixel_size, r * pixel_size
     x, y = 0, r
     delta = 1 - 2 * r
     error = 0
+    print("Circle coordinate") if not flag else None
     while y >= 0:
         pg.draw.rect(sf, BLACK, (disp_x + x, disp_y + y, pixel_size, pixel_size))
         pg.draw.rect(sf, BLACK, (disp_x + x, disp_y - y, pixel_size, pixel_size))
         pg.draw.rect(sf, BLACK, (disp_x - x, disp_y + y, pixel_size, pixel_size))
         pg.draw.rect(sf, BLACK, (disp_x - x, disp_y - y, pixel_size, pixel_size))
+        if not flag:
+            print(f'Point 1 ({(disp_x + x) // pixel_size}, {(disp_y + y) // pixel_size})')
+            print(f'Point 2 ({(disp_x + x) // pixel_size}, {(disp_y - y) // pixel_size})')
+            print(f'Point 3 ({(disp_x - x) // pixel_size}, {(disp_y + y) // pixel_size})')
+            print(f'Point 4 ({(disp_x - x) // pixel_size}, {(disp_y - y) // pixel_size})')
+            print("=" * 8)
 
         error = 2 * (delta + y) - 1
         if delta < 0 and error <= 0:
@@ -64,6 +73,7 @@ def draw_circle(x, y, r):
         delta += 2 * (x - y)
         y -= pixel_size
 
+
 sf = pg.display.set_mode(SIZE)
 clock = pg.time.Clock()
 
@@ -78,8 +88,9 @@ while True:
     sf.fill(LIGHT_GREY)
 
     draw_coord_axies()
-    draw_line(10, 14, 20, 30)
-    draw_circle(60, 50, 9)
+    draw_line(10, 14, 20, 30, flag)
+    draw_circle(60, 50, 9, flag)
+    if not flag: flag = True
 
     mouse_pos = pg.mouse.get_pos()
     pg.draw.line(sf, BLACK, [0, mouse_pos[1]], [WIDTH, mouse_pos[1]], 1)
